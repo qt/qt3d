qt/qt3d

commit 8925e1954e2f25049fd49ba2970b8cc48fba163f

Make no culling the default.
Most of our example apps feature small scenes that don't benefit from
culling anyway.

We do need a proper fix for
http://bugreports.qt.nokia.com/browse/QTBUG-18823 but for now this will
reduce the pain quite a bit.

Change-Id: I8d960e3b17380f60ffa1dad5d7ffffa3c299ee8e

src/threed/scene/qglscenenode.cpp
@@ -181,7 +181,7 @@ QT_BEGIN_NAMESPACE
     This enum defines option flags for QGLSceneNode.
 
     \value CullBoundingBox Perform a cull using boundingBox() before
-        attempting to draw the geometry().  Default is true.
+        attempting to draw the geometry().  Default is false.
     \value ViewNormals Enables the display of lighting normals for
         debugging purposes.  Default is false.
 
@@ -255,7 +255,7 @@ QGLSceneNode::~QGLSceneNode()
 
 /*!
     Returns the drawing options associated with this node.
-    The default is CullBoundingBox.
+    The default is 0 (no options set).
 
     \sa setOptions(), setOption()
 */

src/threed/scene/qglscenenode_p.h
@@ -77,7 +77,7 @@ public:
         , backMaterial(-1)
         , start(0)
         , count(0)
-        , options(QGLSceneNode::CullBoundingBox)
+        , options(0)
         , pickNode(0)
         , boxValid(false)
         , drawingMode(QGL::Triangles)

commit 55bd9d6f2648a9c6a8721ce0eedb7dddec700d84

Fix unit test.
This one got broken by 8925e1954e2f25049fd49ba2970b8cc48fba163f.

Should have been code reviewed...

Change-Id: I059e4d90719b9bd7be11d546ccbbe3e61d91f289

src/threed/scene/qglscenenode.cpp
@@ -180,6 +180,7 @@ QT_BEGIN_NAMESPACE
     \enum QGLSceneNode::Option
     This enum defines option flags for QGLSceneNode.
 
+    \value NoOptions Do not enable any QGLSceneNode options.
     \value CullBoundingBox Perform a cull using boundingBox() before
         attempting to draw the geometry().  Default is false.
     \value ViewNormals Enables the display of lighting normals for

src/threed/scene/qglscenenode.h
@@ -75,6 +75,7 @@ public:
 
     enum Option
     {
+        NoOptions       = 0x0000,
         CullBoundingBox = 0x0001,
         ViewNormals     = 0x0002
     };

tests/auto/threed/qglscenenode/tst_qglscenenode.cpp
@@ -73,7 +73,7 @@ private slots:
 void tst_QGLSceneNode::defaultValues()
 {
     QGLSceneNode node;
-    QVERIFY(node.options() == QGLSceneNode::CullBoundingBox);
+    QVERIFY(node.options() == QGLSceneNode::NoOptions);
     QVERIFY(node.geometry().isNull());
     QVERIFY(node.boundingBox().isNull());
     QVERIFY(node.localTransform().isIdentity());