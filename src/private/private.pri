19075618b9c1ebffa75ba32d895b759f9ea412db
Kalle Lehtonen <kalle.ju.lehtonen@nokia.com>
2011-08-16 16:43:52 +1000

    Private header used from QtCore is missing class export
    Because the export is missing, symbian compilation with RVCT4.0
    fails because of missing symbol. Also the header was unnecessarily
    added to header list of Quick3D which caused qmake to preprocess
    the file.
    
    Change-Id: Ifddee36156e2312edd30e2fef12c58f718361a8f
    Reviewed-on: http://codereview.qt.nokia.com/2989
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Danny Pope <daniel.pope@nokia.com>
    Reviewed-by: Sarah Jane Smith <sarah.j.smith@nokia.com>

--- src/private/private.pri
+++ src/private/private.pri
@@ -1,6 +1,3 @@
 INCLUDEPATH += $$PWD
 VPATH += $$PWD
 
-PRIVATE_HEADERS += \
-    qfactoryloader_p.h \
-    qglextensions_p.h
--- src/private/qfactoryloader_p.h
+++ src/private/qfactoryloader_p.h
@@ -67,7 +67,7 @@ class QFactoryLoaderPrivate;
 class QLibraryPrivate;
 #endif
 
-class QFactoryLoader : public QObject
+class Q_CORE_EXPORT QFactoryLoader : public QObject
 {
     Q_OBJECT
     Q_DECLARE_PRIVATE(QFactoryLoader)
