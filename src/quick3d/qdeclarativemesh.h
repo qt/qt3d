66489e6ab1d46399cc0a09e8746b0260f3256f69
Sarah Smith <sarah.j.smith@nokia.com>
2011-11-28 17:54:19 -0800

    Remove spurious private header warns.
    When these classes were made public in the quick3d library some private
    header warnings were not removed - fix that now.
    
    Change-Id: I9d23dce91be9d7a92b31df91a8deeaa2ae4830ae
    Reviewed-by: Patrick Burke <patrick.burke@nokia.com>
    Reviewed-by: Sarah Jane Smith <sarah.j.smith@nokia.com>

--- src/quick3d/qdeclarativemesh.h
+++ src/quick3d/qdeclarativemesh.h
@@ -39,19 +39,8 @@
 **
 ****************************************************************************/
 
-#ifndef QDECLARATIVEMESH_P_H
-#define QDECLARATIVEMESH_P_H
-
-//
-//  W A R N I N G
-//  -------------
-//
-// This file is not part of the Qt API.  It exists purely as an
-// implementation detail.  This header file may change from version to
-// version without notice, or even be removed.
-//
-// We mean it.
-//
+#ifndef QDECLARATIVEMESH_H
+#define QDECLARATIVEMESH_H
 
 #include "qt3dquickglobal.h"
 
