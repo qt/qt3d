d6a188c6136c33b5079878529d425c0d39d17fe1
Sarah Smith <sarah.j.smith@nokia.com>
2011-11-28 17:54:19 -0800

    Remove spurious private header warns.
    When these classes were made public in the quick3d library some private
    header warnings were not removed - fix that now.
    
    Change-Id: I9d23dce91be9d7a92b31df91a8deeaa2ae4830ae
    Reviewed-by: Patrick Burke <patrick.burke@nokia.com>

--- src/quick3d/qdeclarativeviewport.h
+++ src/quick3d/qdeclarativeviewport.h
@@ -39,19 +39,8 @@
 **
 ****************************************************************************/
 
-#ifndef QDECLARATIVEVIEWPORT_P_H
-#define QDECLARATIVEVIEWPORT_P_H
-
-//
-//  W A R N I N G
-//  -------------
-//
-// This file is not part of the Qt API.  It exists purely as an
-// implementation detail.  This header file may change from version to
-// version without notice, or even be removed.
-//
-// We mean it.
-//
+#ifndef QDECLARATIVEVIEWPORT_H
+#define QDECLARATIVEVIEWPORT_H
 
 #include "qt3dquickglobal.h"
 #include <QtCore/qobject.h>
