32959ce17c71d7629d78c65c6a94889e033b8b21
Thiago Macieira <thiago.macieira@intel.com>
2012-06-22 10:58:09 +0200

    Update the export macros in qt3d.git
    Use the new, simpler QT_STATIC macro.
    
    Change-Id: I59954b6739bd354e547ecd83d291344663afb973
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@nokia.com>

--- src/threed/global/qt3dglobal.h
+++ src/threed/global/qt3dglobal.h
@@ -44,30 +44,14 @@
 
 #include <QtCore/qglobal.h>
 
-// XXX: Move to qglobal.h eventually.
-#if defined(Q_OS_WIN)
-#  if defined(QT_NODLL)
-#    undef QT_MAKEDLL
-#    undef QT_DLL
-#  elif defined(QT_MAKEDLL)        /* create a Qt DLL library */
-#    if defined(QT_DLL)
-#      undef QT_DLL
-#    endif
+#ifndef QT_STATIC
 #    if defined(QT_BUILD_3D_LIB)
 #        define Q_QT3D_EXPORT Q_DECL_EXPORT
 #    else
 #        define Q_QT3D_EXPORT Q_DECL_IMPORT
 #    endif
-#  elif defined(QT_DLL) /* use a Qt DLL library */
-#    define Q_QT3D_EXPORT Q_DECL_IMPORT
-#  endif
-#endif
-#if !defined(Q_QT3D_EXPORT)
-#   if defined(QT_SHARED)
-#       define Q_QT3D_EXPORT Q_DECL_EXPORT
-#   else
+#else
 #       define Q_QT3D_EXPORT
-#   endif
 #endif
 
 #endif
