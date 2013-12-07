8baaf6653a0c407e568beebf1d40329108b44245
Thiago Macieira <thiago.macieira@intel.com>
2013-12-06 21:07:41 -0800

    Fix compilation with ICC 14
    ICC didn't like passing a char array to std::find:
    MD3Loader.cpp(847): error: invalid type conversion: "const char *" to "const char (&)[64]"
                  skins.textures.begin(), skins.textures.end(), pcSurfaces->NAME );
                                                                ^
    
    Change-Id: I65a624c749263b8a28e9d37464ce80df02996354
    Reviewed-by: Andy Maloney <asmaloney@gmail.com>
    Reviewed-by: Alex Blasche <alexander.blasche@digia.com>

--- 3rdparty/assimp/code/MD3Loader.cpp
+++ 3rdparty/assimp/code/MD3Loader.cpp
@@ -843,8 +843,9 @@ void MD3Importer::InternReadFile( const std::string& pFile,
         const char* texture_name = NULL;
 
         // Check whether we have a texture record for this surface in the .skin file
+        const char *surface_name = pcSurfaces->NAME;
         std::list< Q3Shader::SkinData::TextureEntry >::iterator it = std::find(
-            skins.textures.begin(), skins.textures.end(), pcSurfaces->NAME );
+            skins.textures.begin(), skins.textures.end(), surface_name);
 
         if (it != skins.textures.end()) {
             texture_name = &*( _texture_name = (*it).second).begin();
