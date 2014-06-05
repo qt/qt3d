1d3082dd5621834d5f9770facb6228b6b176cff1
Paul Lemire <paul.lemire@kdab.com>
2014-06-05 17:23:20 +0200

    Register RenderPassCriterion in Qt3D.Render
    Change-Id: I85f30632802d516665d7b9c7b4a78be2afd878bb
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>

--- src/quick3d/imports/render/qt3dquick3drenderplugin.cpp
+++ src/quick3d/imports/render/qt3dquick3drenderplugin.cpp
@@ -58,6 +58,7 @@
 #include <Qt3DRenderer/cameraselector.h>
 #include <Qt3DRenderer/effect.h>
 #include <Qt3DRenderer/parameter.h>
+#include <Qt3DRenderer/renderpasscriterion.h>
 #include <Qt3DQuickRenderer/quick3dtechnique.h>
 #include <Qt3DQuickRenderer/quick3dmaterial.h>
 #include <Qt3DQuickRenderer/quick3dtechniquefilter.h>
@@ -79,6 +80,7 @@ void Qt3DQuick3DRenderPlugin::registerTypes(const char *uri)
     qmlRegisterExtendedType<Qt3D::Effect, Qt3D::Render::Quick::Quick3DEffect>(uri, 2, 0, "Effect");
     qmlRegisterExtendedType<Qt3D::Technique, Qt3D::Render::Quick::Quick3DTechnique>(uri, 2, 0, "Technique");
     qmlRegisterType<Qt3D::TechniqueCriterion>(uri, 2, 0, "TechniqueCriterion");
+    qmlRegisterType<Qt3D::RenderPassCriterion>(uri, 2, 0, "RenderPassCriterion");
     qmlRegisterType<Qt3D::Tag>(uri, 2, 0, "Tag");
     qmlRegisterType<Qt3D::Parameter>(uri, 2, 0, "Parameter");
     qmlRegisterType<Qt3D::Mesh>(uri, 2, 0, "Mesh");
