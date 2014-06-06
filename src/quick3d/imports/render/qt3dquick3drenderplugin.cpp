1929b92adf297c3cca3099042043e70e5eaa484d
Paul Lemire <paul.lemire@kdab.com>
2014-06-06 12:05:16 +0200

    RenderPass registered as extended type
    Change-Id: I8e34d6d7c8ac178fe3e982e5af16eb60d110763e
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>

--- src/quick3d/imports/render/qt3dquick3drenderplugin.cpp
+++ src/quick3d/imports/render/qt3dquick3drenderplugin.cpp
@@ -69,6 +69,7 @@
 #include <Qt3DQuickRenderer/quick3dscene.h>
 #include <Qt3DQuickRenderer/quick3dtexture.h>
 #include <Qt3DQuickRenderer/shaderpropertyparser.h>
+#include <Qt3DQuickRenderer/quick3drenderpass.h>
 
 QT_BEGIN_NAMESPACE
 
@@ -85,7 +86,7 @@ void Qt3DQuick3DRenderPlugin::registerTypes(const char *uri)
     qmlRegisterType<Qt3D::Parameter>(uri, 2, 0, "Parameter");
     qmlRegisterType<Qt3D::Mesh>(uri, 2, 0, "Mesh");
     qmlRegisterExtendedType<Qt3D::Material, Qt3D::Render::Quick::Quick3DMaterial>(uri, 2, 0, "Material");
-    qmlRegisterType<Qt3D::RenderPass>(uri, 2, 0, "RenderPass");
+    qmlRegisterExtendedType<Qt3D::RenderPass, Qt3D::Render::Quick::Quick3DRenderPass>(uri, 2, 0, "RenderPass");
     qmlRegisterType<Qt3D::ShaderProgram>(uri, 2, 0, "ShaderProgram");
     qmlRegisterType<Qt3D::Shape>(uri, 2, 0, "Shape");
     qmlRegisterType<Qt3D::Render::Quick::Quick3DTexture>(uri, 2, 0, "Texture");
