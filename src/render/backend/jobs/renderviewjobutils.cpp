qt/qt3d

commit e7443372bc036839c020d372265d4708d2eca4d7

Optimize: Remove temporary QHash for parameters in RenderView.
The QHashes used here are usually very small. In my tests, the
list often only contains 4 items for example. Incurring an
allocation for every node in the graph is very costly. Using a
sorted, stack-allocated QVarLengthArray instead improves the
performance greatly.

Linearly stepping over the small list is also quicker than
hashing strings repeatedly. This can be seen by profiling
the bigscene example before and after this patch with
disabled animations and custom materials for each cylinder as
added in 91a0ee64c850d2efad4b1743afc15a19b18af25e.

Change-Id: Ida370fbb3c9b1c6a0673a112eb2107970b40ea01
Reviewed-by: Paul Lemire <paul.lemire@kdab.com>
Reviewed-by: Sean Harmer <sean.harmer@kdab.com>

src/render/backend/jobs/renderviewjobutils.cpp
@@ -316,37 +316,46 @@ RenderRenderPassList findRenderPassesForTechnique(Renderer *renderer,
     return passes;
 }
 
-static void addParametersForIds(QHash<QString, QVariant> *params, ParameterManager *manager,
+
+ParameterInfoList::iterator findParamInfo(ParameterInfoList *params, const QString &name)
+{
+    ParameterInfoList::iterator it = std::lower_bound(params->begin(), params->end(), name);
+    if (it != params->end() && it->name != name)
+        return params->end();
+    return it;
+}
+
+static void addParametersForIds(ParameterInfoList *params, ParameterManager *manager,
                                 const QList<QNodeId> &parameterIds)
 {
     Q_FOREACH (const QNodeId &paramId, parameterIds) {
         RenderParameter *param = manager->lookupResource(paramId);
-        if (param != Q_NULLPTR)
-            params->insert(param->name(), param->value());
+        if (param != Q_NULLPTR) {
+            ParameterInfoList::iterator it = std::lower_bound(params->begin(), params->end(), param->name());
+            if (it == params->end() || it->name != param->name())
+                params->insert(it, ParameterInfo(param->name(), param->value()));
+        }
     }
 }
 
-QHash<QString, QVariant> parametersFromMaterialEffectTechnique(ParameterManager *manager,
-                                                               RenderMaterial *material,
-                                                               RenderEffect *effect,
-                                                               RenderTechnique *technique)
+void parametersFromMaterialEffectTechnique(ParameterInfoList *infoList,
+                                           ParameterManager *manager,
+                                           RenderMaterial *material,
+                                           RenderEffect *effect,
+                                           RenderTechnique *technique)
 {
-    QHash<QString, QVariant> params;
-
     // Material is preferred over Effect
     // Effect is preferred over Technique
     // By filling the hash in reverse preference order, we ensure that we preserve preference
 
     if (effect)
-        addParametersForIds(&params, manager, effect->parameters());
+        addParametersForIds(infoList, manager, effect->parameters());
 
     if (technique)
-        addParametersForIds(&params, manager, technique->parameters());
+        addParametersForIds(infoList, manager, technique->parameters());
 
     if (material)
-        addParametersForIds(&params, manager, material->parameters());
-
-    return params;
+        addParametersForIds(infoList, manager, material->parameters());
 }
 
 RenderStateSet *buildRenderStateSet(RenderRenderPass *pass, QFrameAllocator *allocator)

src/render/backend/jobs/renderviewjobutils_p.h
@@ -80,16 +80,40 @@ Q_AUTOTEST_EXPORT RenderRenderPassList findRenderPassesForTechnique(Renderer *re
                                                                     RenderView *renderView,
                                                                     RenderTechnique *technique);
 
-Q_AUTOTEST_EXPORT QHash<QString, QVariant> parametersFromMaterialEffectTechnique(ParameterManager *manager,
-                                                                                 RenderMaterial *material,
-                                                                                 RenderEffect *effect,
-                                                                                 RenderTechnique *technique);
+struct ParameterInfo
+{
+    ParameterInfo(const QString &name = QString(), const QVariant &value = QVariant())
+      : name(name)
+      , value(value)
+    {}
+
+    QString name;
+    QVariant value;
+
+    bool operator<(const QString &otherName) const
+    {
+        return name < otherName;
+    }
+};
+
+typedef QVarLengthArray<ParameterInfo, 16> ParameterInfoList;
+
+Q_AUTOTEST_EXPORT void parametersFromMaterialEffectTechnique(ParameterInfoList *infoList,
+                                                             ParameterManager *manager,
+                                                             RenderMaterial *material,
+                                                             RenderEffect *effect,
+                                                             RenderTechnique *technique);
+
+Q_AUTOTEST_EXPORT ParameterInfoList::iterator findParamInfo(ParameterInfoList *infoList,
+                                                            const QString &name);
 
 Q_AUTOTEST_EXPORT RenderStateSet *buildRenderStateSet(RenderRenderPass *pass,
                                                       QFrameAllocator *allocator);
 
 } // namespace Render
 } // namespace Qt3D
+
+Q_DECLARE_TYPEINFO(Qt3D::Render::ParameterInfo, Q_MOVABLE_TYPE);
 
 QT_END_NAMESPACE
 

src/render/backend/renderview.cpp
@@ -68,9 +68,6 @@
 
 #include <Qt3DRenderer/qparametermapping.h>
 
-// TODO: Move out once this is all refactored
-#include <Qt3DRenderer/private/renderviewjobutils_p.h>
-
 #include <Qt3DCore/qentity.h>
 
 #include <QtGui/qsurface.h>
@@ -340,8 +337,8 @@ void RenderView::buildRenderCommands(RenderEntity *node)
                 }
 
                 // Get the parameters for our selected rendering setup
-                QHash<QString, QVariant> parameters =
-                    parametersFromMaterialEffectTechnique(m_renderer->parameterManager(), material, effect, technique);
+                ParameterInfoList parameters;
+                parametersFromMaterialEffectTechnique(&parameters, m_renderer->parameterManager(), material, effect, technique);
 
                 // 1 RenderCommand per RenderPass pass on an Entity with a Mesh
                 Q_FOREACH (RenderRenderPass *pass, passes) {
@@ -403,7 +400,7 @@ void RenderView::buildSortingKey(RenderCommand *command)
     }
 }
 
-void RenderView::setShaderAndUniforms(RenderCommand *command, RenderRenderPass *rPass, QHash<QString, QVariant> &parameters, const QMatrix4x4 &worldTransform)
+void RenderView::setShaderAndUniforms(RenderCommand *command, RenderRenderPass *rPass, ParameterInfoList &parameters, const QMatrix4x4 &worldTransform)
 {
     // The VAO Handle is set directly in the renderer thread so as to avoid having to use a mutex here
     // Set shader, technique, and effect by basically doing :
@@ -456,7 +453,8 @@ void RenderView::setShaderAndUniforms(RenderCommand *command, RenderRenderPass *
 
                 // Set uniforms and attributes explicitly binded
                 Q_FOREACH (QParameterMapping *binding, rPass->bindings()) {
-                    if (!parameters.contains(binding->parameterName())) {
+                    ParameterInfoList::iterator it = findParamInfo(&parameters, binding->parameterName());
+                    if (it == parameters.end()) {
                         if (binding->bindingType() == QParameterMapping::Attribute
                             && attributeNames.contains(binding->shaderVariableName())) {
                             command->m_parameterAttributeToShaderNames.insert(binding->parameterName(), binding->shaderVariableName());
@@ -472,16 +470,17 @@ void RenderView::setShaderAndUniforms(RenderCommand *command, RenderRenderPass *
                             qCWarning(Render::Backend) << Q_FUNC_INFO << "Trying to bind a Parameter that hasn't been defined " << binding->parameterName();
                         }
                     } else {
-                        setUniformValue(command->m_uniforms, binding->shaderVariableName(), parameters.take(binding->parameterName()));
+                        setUniformValue(command->m_uniforms, binding->shaderVariableName(), it->value);
+                        parameters.erase(it);
                     }
                 }
 
                 // If there are remaining parameters, those are set as uniforms
                 if (!uniformNames.isEmpty() && !parameters.isEmpty()) {
-                    QHash<QString, QVariant>::iterator it = parameters.begin();
+                    ParameterInfoList::iterator it = parameters.begin();
                     while (it != parameters.end()) {
-                        if (uniformNames.contains(it.key())) {
-                            setUniformValue(command->m_uniforms, it.key(), it.value());
+                        if (uniformNames.contains(it->name)) {
+                            setUniformValue(command->m_uniforms, it->name, it->value);
                             it = parameters.erase(it);
                         } else {
                             // Else param unused by current shader

src/render/backend/renderview_p.h
@@ -53,6 +53,9 @@
 
 #include <Qt3DCore/qframeallocator.h>
 
+// TODO: Move out once this is all refactored
+#include <Qt3DRenderer/private/renderviewjobutils_p.h>
+
 #include <QVector>
 #include <QMutex>
 #include <QColor>
@@ -191,7 +194,7 @@ public:
     void addSortCriteria(const QList<QNodeId> &sortMethodUid) { m_data->m_sortingCriteria.append(sortMethodUid); }
 
 private:
-    void setShaderAndUniforms(RenderCommand *command, RenderRenderPass *pass, QHash<QString, QVariant> &parameters, const QMatrix4x4 &worldTransform);
+    void setShaderAndUniforms(RenderCommand *command, RenderRenderPass *pass, ParameterInfoList &parameters, const QMatrix4x4 &worldTransform);
 
     Renderer *m_renderer;
     QFrameAllocator *m_allocator;

commit 1e13c045df0cf380ea37f962e1f108ba365793a9

Fix parameter priority regression
Commit e7443372bc036839c020d372265d4708d2eca4d7 accidentally introduced
a regression in the construction of the list of parameters for a shader.

In that commit addParametersForIds() was changed to not insert a
parameter if a parameter with the same name was already in the list.
This changed the semantics of the function but the calling order wasn't
updated accordingly.

This fixes the regression by filling the list of parameters in the
correct priority order.

Change-Id: I5a33a137c40fe463d42e8459dc197b295f248e63
Reviewed-by: Paul Lemire <paul.lemire@kdab.com>

src/render/backend/jobs/renderviewjobutils.cpp
@@ -347,18 +347,22 @@ void parametersFromMaterialEffectTechnique(ParameterInfoList *infoList,
                                            RenderEffect *effect,
                                            RenderTechnique *technique)
 {
-    // Material is preferred over Effect
-    // Effect is preferred over Technique
-    // By filling the hash in reverse preference order, we ensure that we preserve preference
-
-    if (effect)
-        addParametersForIds(infoList, manager, effect->parameters());
+    // The parameters are taken in the following priority order:
+    //
+    // 1) Material
+    // 2) Technique
+    // 3) Effect
+    //
+    // That way a user can override defaults in Effect's and Techniques on a
+    // object manner and a Technique can override global defaults from the Effect.
+    if (material)
+        addParametersForIds(infoList, manager, material->parameters());
 
     if (technique)
         addParametersForIds(infoList, manager, technique->parameters());
 
-    if (material)
-        addParametersForIds(infoList, manager, material->parameters());
+    if (effect)
+        addParametersForIds(infoList, manager, effect->parameters());
 }
 
 RenderStateSet *buildRenderStateSet(RenderRenderPass *pass, QFrameAllocator *allocator)