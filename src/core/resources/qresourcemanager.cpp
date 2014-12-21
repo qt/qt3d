78a6cba032b177d6d6fd5b559112e177108ff532
Sean Harmer <sean.harmer@kdab.com>
2014-12-21 18:17:03 +0000

    Make QResourcemanager and friends docs more consistent
    Change-Id: I0e9e860568b88ced321cdd35938967b1a3986ee5
    Reviewed-by: Paul Lemire <paul.lemire@kdab.com>

--- src/core/resources/qresourcemanager.cpp
+++ src/core/resources/qresourcemanager.cpp
@@ -40,69 +40,65 @@
 ****************************************************************************/
 
 /*!
- * \class QResourceManager
- *
- * \brief The QResourceManager allocates memory for resources that can be referenced by a
- * QHandle.
- *
- * Using a QHandleManager for handle management, the QResourceManager's responsibility is
- * to provide memory for resources and to offer ways to interact with the resource through
- * the QHandle.
- *
- * Using the QHandle obtained when acquiring a resource, the resource can be retrieved and
- * released when not needed any longer.
- *
- * Internally, memory can be reorganized for best performances while being transparent to the user.
- *
- * The memory allocation scheme and locking policies can be customized by providing template
- * parameters. The defaults are ArrayAllocationPolicy and NonLockingPolicy respectively.
- *
- * \since 5.3
- * \namespace Qt3D
- */
+    \class Qt3D::QResourceManager
+    \inmodule Qt3DCore
+    \since 5.5
+
+    \brief The QResourceManager allocates memory for resources that can be referenced by a QHandle.
+
+    Using a QHandleManager for handle management, the QResourceManager's responsibility is
+    to provide memory for resources and to offer ways to interact with the resource through
+    the QHandle.
+
+    Using the QHandle obtained when acquiring a resource, the resource can be retrieved and
+    released when no longer needed.
+
+    Internally, memory can be reorganized for best performance while being transparent to the user.
+
+    The memory allocation scheme and locking policies can be customized by providing template
+    parameters. The defaults are ArrayAllocationPolicy and NonLockingPolicy respectively.
+*/
 
 /*!
- *
- * \class ArrayAllocatingPolicy
- *
- * \brief Allocates memory in a contiguous space trying to
- * minimize fragmentation and cache misses.
- *
- * Once the maximum number of entities is reached, no more allocations can be made until some resources are
- * released
- *
- * \since 5.3
- * \namespace Qt3D
- * \sa QResourceManager
- * \sa ListAllocatingPolicy
- */
+    \class Qt3D::ArrayAllocatingPolicy
+    \inmodule Qt3DCore
+    \since 5.5
+
+    \brief Allocates memory in a contiguous space trying to minimize fragmentation and cache misses.
+
+    Once the maximum number of entities is reached, no more allocations can be made until some resources are
+    released
+
+    \sa QResourceManager
+    \sa ListAllocatingPolicy
+*/
 
 /*!
- *
- * \class ListAllocatingPolicy
- *
- * \brief Allocates resources in a list.
- *
- * It is best to use it when you don't need to iterate over an entire set of resources, in which
- * case ArrayAllocatingPolicy is faster. It can store a non predefined amount of resources, though
- * there might not be enough handles at some point, depending on the INDEXBITS used.
- * It's main use case is to manage resources that are accessed in an independent manner from other
- * resources of the same type.
- *
- * \since 5.3
- * \namespace Qt3D
- * \sa QResourceManager
- * \sa ArrayAllocatingPolicy
- */
+    \class Qt3D::ListAllocatingPolicy
+    \inmodule Qt3DCore
+    \since 5.5
+
+    \brief Allocates resources in a list.
+
+    It is best to use it when you don't need to iterate over an entire set of resources, in which
+    case ArrayAllocatingPolicy is faster. It can store a non predefined amount of resources, though
+    there might not be enough handles at some point, depending on the INDEXBITS used.
+    It's main use case is to manage resources that are accessed in an independent manner from other
+    resources of the same type.
+
+    \sa QResourceManager
+    \sa ArrayAllocatingPolicy
+*/
 
 /*!
- * \class ObjectLevelLockingPolicy
- *
- * \brief Provided locking access to a resource through the use of a QReadWriteLock.
- *
- * This policy should be used in a QResourceManager when multiple thread might access the manager for
- * read or write operations at the same time.
- *
- * It provides two convenience classes WriteLocker and ReadLocker that behave like QReadLocker and QWriteLocker.
- *
- */
+    \class Qt3D::ObjectLevelLockingPolicy
+    \inmodule Qt3DCore
+    \since 5.5
+
+    \brief Provided locking access to a resource through the use of a QReadWriteLock.
+
+    This policy should be used in a QResourceManager when multiple threads may access the manager for
+    read and write operations at the same time.
+
+    It provides two convenience classes WriteLocker and ReadLocker that behave like QReadLocker and QWriteLocker.
+*/
