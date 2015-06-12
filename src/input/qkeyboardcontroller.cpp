6f93508bcd38d8d0fa524ac00d65b518fac0d82a
Topi Reinio <topi.reinio@digia.com>
2015-06-12 15:53:11 +0200

    Doc: Add skeleton docs for Qt3D Input classes and QML types
    Not fully documented yet, but enables the classes and QML types to be
    visible in the documentation.
    
    Change-Id: I557aeae9ff6314b697bcc9313c1061543363b645
    Reviewed-by: Leena Miettinen <riitta-leena.miettinen@theqtcompany.com>
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>

--- src/input/qkeyboardcontroller.cpp
+++ src/input/qkeyboardcontroller.cpp
@@ -53,10 +53,29 @@ QKeyboardControllerPrivate::QKeyboardControllerPrivate()
 {
 }
 
+/*!
+    \class Qt3D::QKeyboardController
+    \inmodule Qt3DInput
+    \since 5.5
+*/
+
+/*!
+    \qmltype KeyboardController
+    \inqmlmodule Qt3D.Input
+    \since 5.5
+    \instantiates Qt3D::QKeyboardController
+    \inherits Node
+*/
+
 QKeyboardController::QKeyboardController(QNode *parent)
     : QNode(*new QKeyboardControllerPrivate, parent)
 {
 }
+
+/*!
+    \qmlproperty KeyboardInput Qt3D.Input::KeyboardController::activeInput
+    \readonly
+*/
 
 QKeyboardInput *QKeyboardController::activeInput() const
 {
