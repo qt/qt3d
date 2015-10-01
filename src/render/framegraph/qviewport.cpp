e249ad14e86dd02bca599af0d13961e09097a55b
Paul Lemire <paul.lemire@kdab.com>
2015-10-01 16:19:53 +0200

    QViewport fix: properly copy color when cloning
    Change-Id: I4cd0fa73e06224b023215cef57efbb6b2fbbc30d
    Reviewed-by: Andy Nichols <andy.nichols@theqtcompany.com>

--- src/render/framegraph/qviewport.cpp
+++ src/render/framegraph/qviewport.cpp
@@ -58,6 +58,7 @@ void QViewport::copy(const QNode *ref)
     QFrameGraphNode::copy(ref);
     const QViewport *viewport = static_cast<const QViewport*>(ref);
     d_func()->m_rect = viewport->d_func()->m_rect;
+    d_func()->m_clearColor = viewport->d_func()->m_clearColor;
 }
 
 QViewport::QViewport(QNode *parent)
