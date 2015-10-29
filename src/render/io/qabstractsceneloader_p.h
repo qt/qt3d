c59e819c711414eb62f3a1ccf43e018601b8918e
Andy Nichols <andy.nichols@theqtcompany.com>
2015-10-29 17:49:09 +0100

    Qt3DRender::QAbstractSceneLoaderPrivate: Remove unnecessary export
    Change-Id: If8b16f7a9d638f3dc18f719652647297d09842dc
    Reviewed-by: Paul Lemire <paul.lemire@kdab.com>

--- src/render/io/qabstractsceneloader_p.h
+++ src/render/io/qabstractsceneloader_p.h
@@ -49,7 +49,6 @@
 //
 
 #include <private/qcomponent_p.h>
-#include <Qt3DRender/qt3drender_global.h>
 #include <Qt3DRender/qabstractsceneloader.h>
 
 QT_BEGIN_NAMESPACE
@@ -58,7 +57,7 @@ namespace Qt3DRender {
 
 class AbstractSceneParser;
 
-class QT3DRENDERSHARED_EXPORT QAbstractSceneLoaderPrivate : public Qt3DCore::QComponentPrivate
+class QAbstractSceneLoaderPrivate : public Qt3DCore::QComponentPrivate
 {
 public:
     QAbstractSceneLoaderPrivate();
