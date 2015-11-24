e00db5f03c9effd21e9df23eb6da2be8de024776
Paul Lemire <paul.lemire@kdab.com>
2015-11-24 09:54:25 +0100

    Register QFrustumCulling to QML
    Change-Id: If63b35a24b05a8279edfff48c893fce5a37d63de
    Reviewed-by: Andy Nichols <andy.nichols@theqtcompany.com>

--- src/quick3d/imports/render/qt3dquick3drenderplugin.cpp
+++ src/quick3d/imports/render/qt3dquick3drenderplugin.cpp
@@ -101,6 +101,7 @@
 #include <Qt3DRender/qobjectpicker.h>
 #include <Qt3DRender/qboundingvolumespecifier.h>
 #include <Qt3DRender/qboundingvolumedebug.h>
+#include <Qt3DRender/qfrustumculling.h>
 #include <Qt3DQuickRender/private/quick3dtechnique_p.h>
 #include <Qt3DQuickRender/private/quick3dmaterial_p.h>
 #include <Qt3DQuickRender/private/quick3dtechniquefilter_p.h>
@@ -252,6 +253,7 @@ void Qt3DQuick3DRenderPlugin::registerTypes(const char *uri)
     Qt3DRender::Quick::registerExtendedType<Qt3DRender::QStateSet, Qt3DRender::Render::Quick::Quick3DStateSet>("QStateSet", "Qt3D.Render/StateSet", uri, 2, 0, "StateSet");
     qmlRegisterType<Qt3DRender::QNoDraw>(uri, 2, 0, "NoDraw");
     qmlRegisterType<Qt3DRender::QFrameGraph>(uri, 2, 0, "FrameGraph");
+    qmlRegisterType<Qt3DRender::QFrustumCulling>(uri, 2, 0, "FrustumCulling");
 
     // RenderTarget
     qmlRegisterType<Qt3DRender::QRenderAttachment>(uri, 2, 0, "RenderAttachment");
