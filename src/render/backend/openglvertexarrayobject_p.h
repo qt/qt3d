101aac9947b45e83e61935899eb1ede6064571cf
Laszlo Agocs <laszlo.agocs@theqtcompany.com>
2015-11-25 10:29:15 +0100

    Add we mean it to the vao private header
    Change-Id: I97908683dafe265f0d9f2e159362ee0c52488d7d
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>

--- src/render/backend/openglvertexarrayobject_p.h
+++ src/render/backend/openglvertexarrayobject_p.h
@@ -37,6 +37,17 @@
 #ifndef OPENGLVERTEXARRAYOBJECT_H
 #define OPENGLVERTEXARRAYOBJECT_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists for the convenience
+// of other Qt classes.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <QtGui/qopenglvertexarrayobject.h>
 
 QT_BEGIN_NAMESPACE
