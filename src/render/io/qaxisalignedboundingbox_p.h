f4520cf447eb7344ffda703ac4d7f354910a4f4a
Laszlo Agocs <laszlo.agocs@theqtcompany.com>
2015-12-15 15:36:04 +0100

    Add we mean it headers
    Change-Id: I0ad651a68441d3cd0f836c37304cdcb1effac455
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>

--- src/render/io/qaxisalignedboundingbox_p.h
+++ src/render/io/qaxisalignedboundingbox_p.h
@@ -37,6 +37,17 @@
 #ifndef QT3DRENDER_QAXISALIGNEDBOUNDINGBOX_P_H
 #define QT3DRENDER_QAXISALIGNEDBOUNDINGBOX_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists for the convenience
+// of other Qt classes.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <QMatrix4x4>
 #include <QVector>
 #include <QVector3D>
