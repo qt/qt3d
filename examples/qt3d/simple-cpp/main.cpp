f95d24ce9003282841f97e44c1c3fc3b17f86d00
Robert Brock <robert.brock@kdab.com>
2016-01-05 09:50:32 +0000

    Camera moved - simple-cpp
    From z: -40 to z: 40
    
    Change-Id: I67506d849dd80043109a7aeb431348b09c21755a
    Task-number: QTBUG-49800
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>

--- examples/qt3d/simple-cpp/main.cpp
+++ examples/qt3d/simple-cpp/main.cpp
@@ -139,7 +139,7 @@ int main(int argc, char* argv[])
     Qt3DCore::QCamera *cameraEntity = view.defaultCamera();
 
     cameraEntity->lens()->setPerspectiveProjection(45.0f, 16.0f/9.0f, 0.1f, 1000.0f);
-    cameraEntity->setPosition(QVector3D(0, 0, -40.0f));
+    cameraEntity->setPosition(QVector3D(0, 0, 40.0f));
     cameraEntity->setUpVector(QVector3D(0, 1, 0));
     cameraEntity->setViewCenter(QVector3D(0, 0, 0));
     input->setCamera(cameraEntity);
