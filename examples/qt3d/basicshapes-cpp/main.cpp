e1065f1e54848d5a86fbc02839b75696c337a6be
Robert Brock <robert.brock@kdab.com>
2016-01-05 08:45:26 +0000

    Camera moved - basicshapes-cpp
    From z: -20 to z: 20
    
    Change-Id: I5823541ee6a062f457595bc28101fb33809f8e7f
    Task-number: QTBUG-49800
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>

--- examples/qt3d/basicshapes-cpp/main.cpp
+++ examples/qt3d/basicshapes-cpp/main.cpp
@@ -103,7 +103,7 @@ int main(int argc, char **argv)
     cameraEntity->setObjectName(QStringLiteral("cameraEntity"));
 
     cameraEntity->lens()->setPerspectiveProjection(45.0f, 16.0f/9.0f, 0.1f, 1000.0f);
-    cameraEntity->setPosition(QVector3D(0, 0, -20.0f));
+    cameraEntity->setPosition(QVector3D(0, 0, 20.0f));
     cameraEntity->setUpVector(QVector3D(0, 1, 0));
     cameraEntity->setViewCenter(QVector3D(0, 0, 0));
     input->setCamera(cameraEntity);
