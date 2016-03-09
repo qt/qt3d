59bcae5572de6d8e2dc8c610e120cc3fe4162619
Sean Harmer <sean.harmer@kdab.com>
2016-03-09 13:41:35 +0000

    Add We mean it warnings to a couple of private headers
    Change-Id: I5a4ba6357514e6625af40e9a05d58c906f0fae8b
    Reviewed-by: Volker Krause <volker.krause@kdab.com>
    Reviewed-by: Paul Lemire <paul.lemire@kdab.com>

--- src/render/jobs/job_common_p.h
+++ src/render/jobs/job_common_p.h
@@ -40,6 +40,17 @@
 #ifndef QT3DRENDER_RENDER_JOB_COMMON_P_H
 #define QT3DRENDER_RENDER_JOB_COMMON_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <Qt3DCore/private/qaspectjob_p.h>
 
 QT_BEGIN_NAMESPACE
