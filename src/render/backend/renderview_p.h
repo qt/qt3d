782ffe1f81dba9fb68a3b6592c2e90dea8d3057d
Volker Krause <volker.krause@kdab.com>
2016-04-09 17:49:26 +0200

    Set a valid surface size.
    This fixes the viewportMatrix uniform value, which fixes the tessellation
    and wireframe examples.
    
    Change-Id: Ic2a7bba66167673b026caac8461c4f38c68612f2
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>

--- src/render/backend/renderview_p.h
+++ src/render/backend/renderview_p.h
@@ -66,6 +66,7 @@
 #include <Qt3DRender/private/renderviewjobutils_p.h>
 
 #include <QVector>
+#include <QSurface>
 #include <QMutex>
 #include <QColor>
 
@@ -244,7 +245,7 @@ public:
 
     void addSortCriteria(const QVector<Qt3DCore::QNodeId> &sortMethodUid) { m_data->m_sortingCriteria.append(sortMethodUid); }
 
-    void setSurface(QSurface *surface) { m_surface = surface; }
+    void setSurface(QSurface *surface) { m_surface = surface; m_surfaceSize = surface->size(); }
     QSurface *surface() const { return m_surface; }
 
     // Helps making the size of RenderView smaller
