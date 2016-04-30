9a3bb0a02cdfdff836969e8e8b83d8f16e7a4ded
Paul Lemire <paul.lemire350@gmail.com>
2016-04-30 12:23:54 +0200

    Add we mean it to QGraphicsApiFilter header
    Change-Id: I16eb300253464c2b5501686791d3606830c6be12
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>

--- src/render/materialsystem/qgraphicsapifilter_p.h
+++ src/render/materialsystem/qgraphicsapifilter_p.h
@@ -43,6 +43,17 @@
 #include <private/qobject_p.h>
 #include <Qt3DRender/qgraphicsapifilter.h>
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists for the convenience
+// of other Qt classes.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 QT_BEGIN_NAMESPACE
 
 namespace Qt3DRender {
