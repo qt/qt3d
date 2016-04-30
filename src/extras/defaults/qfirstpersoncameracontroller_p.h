c5c7f766ade806a29d159412e806ae2b7b47fb91
Paul Lemire <paul.lemire350@gmail.com>
2016-04-30 12:27:29 +0200

    Add we mean it to qfirstpersoncameracontroller_p.h
    Change-Id: I38489c9029e563aa7f140e0c6fc2151a8341b502
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>

--- src/extras/defaults/qfirstpersoncameracontroller_p.h
+++ src/extras/defaults/qfirstpersoncameracontroller_p.h
@@ -55,6 +55,17 @@
 #include <QVector3D>
 #include "qfirstpersoncameracontroller.h"
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists for the convenience
+// of other Qt classes.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 QT_BEGIN_NAMESPACE
 
 namespace Qt3DRender {
