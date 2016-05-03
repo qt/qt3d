c1374298f22516fbaba6f091bf1b528ba8d09255
Marc Mutz <marc.mutz@kdab.com>
2016-05-03 15:10:47 +0200

    GLTFIO: claim copyright for the substantial recent changes
    Change-Id: Ie2e57e27d7a2c181d0620e76ccf1c11d9dabfcce
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>

--- src/plugins/sceneparsers/gltf/gltfio.h
+++ src/plugins/sceneparsers/gltf/gltfio.h
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2014 Klaralvdalens Datakonsult AB (KDAB).
+** Copyright (C) 2014-2016 Klarälvdalens Datakonsult AB (KDAB).
 ** Copyright (C) 2016 The Qt Company Ltd and/or its subsidiary(-ies).
 ** Contact: https://www.qt.io/licensing/
 **
