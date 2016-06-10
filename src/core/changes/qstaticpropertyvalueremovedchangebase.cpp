f0d75630f45bc79f6c325c76f60aed205b60e1cf
Edward Welbourne <edward.welbourne@qt.io>
2016-06-10 17:43:39 +0200

    QStaticPropertyValueRemovedChangeBasePrivate: initialize m_propertyName
    Coverity (CID 161414) justly grumbles.
    
    Change-Id: I3ffd8e36c5ddef68d8e792e1d802138aa155752e
    Reviewed-by: Kevin Ottens <kevin.ottens@kdab.com>

--- src/core/changes/qstaticpropertyvalueremovedchangebase.cpp
+++ src/core/changes/qstaticpropertyvalueremovedchangebase.cpp
@@ -45,7 +45,8 @@ QT_BEGIN_NAMESPACE
 namespace Qt3DCore {
 
 QStaticPropertyValueRemovedChangeBasePrivate::QStaticPropertyValueRemovedChangeBasePrivate()
-    : QPropertyValueRemovedChangeBasePrivate()
+    : QPropertyValueRemovedChangeBasePrivate(),
+      m_propertyName(nullptr)
 {
 }
 
