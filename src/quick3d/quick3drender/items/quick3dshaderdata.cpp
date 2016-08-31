5688464a384a6cb1b233bc53a3fa604ae9143dd8
Tomi Korpipää <tomi.korpipaa@qt.io>
2016-08-31 10:19:41 +0300

    MSVC2015_64 "internal compiler error" fixes
    Change-Id: I8b67b6932eca7ada6ac44521b8a1a526eb0553e5
    Reviewed-by: Paul Lemire <paul.lemire@kdab.com>
    Reviewed-by: Antti Määttä <antti.maatta@qt.io>

--- src/quick3d/quick3drender/items/quick3dshaderdata.cpp
+++ src/quick3d/quick3drender/items/quick3dshaderdata.cpp
@@ -37,11 +37,11 @@
 **
 ****************************************************************************/
 
-#include "quick3dshaderdata_p.h"
-#include <private/qshaderdata_p.h>
-
 #include <Qt3DQuickRender/private/quick3dshaderdataarray_p.h>
 #include <QMetaProperty>
+
+#include "quick3dshaderdata_p.h"
+#include <private/qshaderdata_p.h>
 
 QT_BEGIN_NAMESPACE
 
