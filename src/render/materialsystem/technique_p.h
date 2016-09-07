be117e458fa7c099c7b88446f549d4aac1fd3526
Paul Lemire <paul.lemire@kdab.com>
2016-09-07 16:30:27 +0200

    Technique: private export for benchmarks
    Change-Id: Ie2e387626ab6e2100ddc4e692348111508869f7a
    Reviewed-by: Paul Lemire <paul.lemire@kdab.com>

--- src/render/materialsystem/technique_p.h
+++ src/render/materialsystem/technique_p.h
@@ -73,7 +73,7 @@ namespace Render {
 
 class TechniqueManager;
 
-class Q_AUTOTEST_EXPORT Technique : public BackendNode
+class QT3DRENDERSHARED_PRIVATE_EXPORT Technique : public BackendNode
 {
 public:
     Technique();
