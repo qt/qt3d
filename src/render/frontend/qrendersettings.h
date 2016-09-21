2b851526db630e7a55f90b7216fbea44cc904bd0
Kevin Ottens <kevin.ottens@kdab.com>
2016-09-21 11:20:47 +0200

    Add namespace to picking settings property type
    Needed so that QML and qmlplugindump process properly the type
    information of this property
    
    Change-Id: I8d4075f481176efa1a50d5d0e418529c893c49eb
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>

--- src/render/frontend/qrendersettings.h
+++ src/render/frontend/qrendersettings.h
@@ -55,7 +55,7 @@ class QT3DRENDERSHARED_EXPORT QRenderSettings : public Qt3DCore::QComponent
 {
     Q_OBJECT
 
-    Q_PROPERTY(QPickingSettings* pickingSettings READ pickingSettings CONSTANT)
+    Q_PROPERTY(Qt3DRender::QPickingSettings* pickingSettings READ pickingSettings CONSTANT)
     Q_PROPERTY(RenderPolicy renderPolicy READ renderPolicy WRITE setRenderPolicy NOTIFY renderPolicyChanged)
     Q_PROPERTY(Qt3DRender::QFrameGraphNode *activeFrameGraph READ activeFrameGraph WRITE setActiveFrameGraph NOTIFY activeFrameGraphChanged)
     Q_CLASSINFO("DefaultProperty", "activeFrameGraph")
