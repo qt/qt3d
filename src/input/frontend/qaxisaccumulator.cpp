40f60c66ab538229f4f64ea8f56682ae33c5926c
Nico Vertriest <nico.vertriest@qt.io>
2016-10-26 13:20:40 +0200

    Doc: corrected several qdoc errors qt3d
    qaxisaccumulator.cpp:182: warning: QML property documented multiple times: 'real Qt3D.Input::AxisAccumulator::value'
    Errors on \return
    
    Change-Id: Ibff02113e1a4ac3cfc79d23960812345cba513cb
    Reviewed-by: Martin Smith <martin.smith@qt.io>

--- src/input/frontend/qaxisaccumulator.cpp
+++ src/input/frontend/qaxisaccumulator.cpp
@@ -183,11 +183,13 @@ QAxisAccumulator::SourceAxisType QAxisAccumulator::sourceAxisType() const
 /*!
     \qmlproperty real Qt3D.Input::AxisAccumulator::value
 
-    The accumulated (integrated) value.
+    The amount to scale the axis value by when accumulating. This can be
+    thought of as the maximum velocity or acceleration the axis can
+    control.
  */
 
 /*!
-   \return the accumulated (integrated) value.
+   Returns the accumulated (integrated) value.
  */
 float QAxisAccumulator::value() const
 {
@@ -206,14 +208,6 @@ float QAxisAccumulator::velocity() const
     Q_D(const QAxisAccumulator);
     return d->m_velocity;
 }
-
-/*!
-    \qmlproperty real Qt3D.Input::AxisAccumulator::value
-
-    The amount to scale the axis value by when accumulating. This can be
-    thought of as the maximum velocity or acceleration the axis can
-    control.
- */
 
 /*!
     The amount to scale the axis value by when accumulating. This can be
