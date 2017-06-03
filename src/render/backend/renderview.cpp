ab0a43e9efba3da95da2ebc7662a1437d5e3a899
Thiago Macieira <thiago.macieira@intel.com>
2017-06-03 12:07:06 -0700

    Initialize member variable (found by GCC 7)
     shaderparameterpack_p.h: In function ‘void Qt3DRender::Render::RenderView::setUniformBlockValue(Qt3DRender::Render::ShaderParameterPack&, Qt3DRender::Render::Shader*, const Qt3DRender::Render::ShaderUniformBlock&, const Qt3DRender::Render::UniformValue&) const’:
     shaderparameterpack_p.h:76:8: error: ‘uniformBlockUBO.Qt3DRender::Render::BlockToUBO::m_needsUpdate’ may be used uninitialized in this function [-Werror=maybe-uninitialized]
    
    Change-Id: Ia3e896da908f42939148fffd14c4b22aab21f0ef
    Reviewed-by: Paul Lemire <paul.lemire@kdab.com>

--- src/render/backend/renderview.cpp
+++ src/render/backend/renderview.cpp
@@ -585,6 +585,7 @@ void RenderView::setUniformBlockValue(ShaderParameterPack &uniformPack,
             BlockToUBO uniformBlockUBO;
             uniformBlockUBO.m_blockIndex = block.m_index;
             uniformBlockUBO.m_bufferID = buffer->peerId();
+            uniformBlockUBO.m_needsUpdate = false;
             uniformPack.setUniformBuffer(std::move(uniformBlockUBO));
             // Buffer update to GL buffer will be done at render time
         }
