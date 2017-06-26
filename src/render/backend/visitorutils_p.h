af7af4c207cf95d537dcb0d70d312f2c97801a43
Sean Harmer <sean.harmer@kdab.com>
2017-06-26 10:47:18 +0100

    Tidy up visitorutils_p_h
    Add missing we mean it warning and include QtGlobal header for the
    QT_BEGIN/END_NAMESPACE macros.
    
    Change-Id: Ife650504694bae532cf2b3070ea179cb0ed8d3b4
    Reviewed-by: Kevin Ottens <kevin.ottens@kdab.com>

--- src/render/backend/visitorutils_p.h
+++ src/render/backend/visitorutils_p.h
@@ -37,6 +37,22 @@
 **
 ****************************************************************************/
 
+#ifndef QT3DRENDER_VISITORUTILS_P_H
+#define QT3DRENDER_VISITORUTILS_P_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists for the convenience
+// of other Qt classes.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include <QtGlobal>
+
 QT_BEGIN_NAMESPACE
 
 namespace Qt3DRender {
@@ -159,3 +175,5 @@ void visitPrimitives(NodeManagers *manager, const GeometryRenderer *renderer, Vi
 } // namespace Qt3DRender
 
 QT_END_NAMESPACE
+
+#endif // QT3DRENDER_VISITORUTILS_P_H
