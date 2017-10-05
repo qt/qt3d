62ade2897b0f6d1211e66e5b1afeb136e7c2a5bd
Sean Harmer <sean.harmer@kdab.com>
2017-10-05 15:09:14 +0100

    Fix dolly in/out speed calculation
    Change-Id: Ie7fe80039a6cb88a0f1540ce252c9dee36d0afe1
    Reviewed-by: Paul Lemire <paul.lemire@kdab.com>

--- src/extras/defaults/qorbitcameracontroller.cpp
+++ src/extras/defaults/qorbitcameracontroller.cpp
@@ -182,7 +182,7 @@ void QOrbitCameraController::moveCamera(const QAbstractCameraController::InputSt
     } else if (state.shiftKeyActive) {
         if (zoomDistance(camera()->position(), theCamera->viewCenter()) > d->m_zoomInLimit * d->m_zoomInLimit) {
             // Dolly
-            theCamera->translate(QVector3D(0, 0, state.tyAxisValue), theCamera->DontTranslateViewCenter);
+            theCamera->translate(QVector3D(0, 0, state.tyAxisValue * linearSpeed() * dt), theCamera->DontTranslateViewCenter);
         } else {
             theCamera->translate(QVector3D(0, 0, -0.5), theCamera->DontTranslateViewCenter);
         }
