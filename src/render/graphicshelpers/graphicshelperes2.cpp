f76259554244bf3fa3045e58c1f59f3eac1b6a03
David Cremoux <david@adeko.com>
2018-02-12 16:37:47 +0300

    Fix regression crash on Windows when ES2 is used
    Using OpenGL ES 2.0 cause a division by zero due to a modification to
    GraphicsHelperES2: uniform.m_rawByteSize is not correctly initialized.
    
    Task-number: QTBUG-64964
    Change-Id: Ib500b683b1402d0160a5e18236ac5b0a4cbf131a
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>

--- src/render/graphicshelpers/graphicshelperes2.cpp
+++ src/render/graphicshelpers/graphicshelperes2.cpp
@@ -212,6 +212,7 @@ QVector<ShaderUniform> GraphicsHelperES2::programUniformsAndLocations(GLuint pro
         // Work around for uniform array names that aren't returned with [0] by some drivers
         if (uniform.m_size > 1 && !uniform.m_name.endsWith(QLatin1String("[0]")))
             uniform.m_name.append(QLatin1String("[0]"));
+        uniform.m_rawByteSize = uniformByteSize(uniform);
         uniforms.append(uniform);
     }
     return uniforms;
