5f417b9fd3625cd229ef02a3d1a12795a58bd468
Paul Lemire <paul.lemire@kdab.com>
2019-09-19 09:00:30 +0200

    QPickEvent: add revisions and fix qml registration
    Task-number: QTBUG-77859
    Change-Id: Ie6fb8ff528b0a8a095b4a49521c94e50de935833
    Reviewed-by: Mike Krus <mike.krus@kdab.com>

--- src/quick3d/imports/render/qt3dquick3drenderplugin.cpp
+++ src/quick3d/imports/render/qt3dquick3drenderplugin.cpp
@@ -239,6 +239,7 @@ void Qt3DQuick3DRenderPlugin::registerTypes(const char *uri)
     qmlRegisterType<Qt3DRender::QObjectPicker, 9>(uri, 2, 9, "ObjectPicker");
     qmlRegisterType<Qt3DRender::QObjectPicker, 13>(uri, 2, 13, "ObjectPicker");
     qmlRegisterUncreatableType<Qt3DRender::QPickEvent>(uri, 2, 0, "PickEvent", QStringLiteral("Events cannot be created"));
+    qmlRegisterUncreatableType<Qt3DRender::QPickEvent, 14>(uri, 2, 14, "PickEvent", QStringLiteral("Events cannot be created"));
     qmlRegisterType<Qt3DRender::Render::Quick::Quick3DRayCaster>(uri, 2, 11, "RayCaster");
     qmlRegisterType<Qt3DRender::Render::Quick::Quick3DScreenRayCaster>(uri, 2, 11, "ScreenRayCaster");
 
