2dabe5fc1e27e7d0538ee6d28806e7aa56c9fc53
Mike Krus <mike.krus@kdab.com>
2019-10-01 14:15:40 +0100

    Add accessor to retrieve frontend node from an id
    Useful later to sync backend changes back to the frontend.
    
    Change-Id: I874d140fc97462b8e186b66c882647db26d5678e
    Reviewed-by: Paul Lemire <paul.lemire@kdab.com>

--- src/core/aspects/qaspectmanager.cpp
+++ src/core/aspects/qaspectmanager.cpp
@@ -62,6 +62,7 @@
 #include <Qt3DCore/private/qtickclockservice_p.h>
 #include <Qt3DCore/private/qnodevisitor_p.h>
 #include <Qt3DCore/private/qnode_p.h>
+#include <Qt3DCore/private/qscene_p.h>
 
 #include <QtCore/QCoreApplication>
 
@@ -377,6 +378,24 @@ QServiceLocator *QAspectManager::serviceLocator() const
 void QAspectManager::setPostConstructorInit(NodePostConstructorInit *postConstructorInit)
 {
     m_postConstructorInit = postConstructorInit;
+}
+
+QNode *QAspectManager::lookupNode(QNodeId id) const
+{
+    if (!m_root)
+        return nullptr;
+
+    QNodePrivate *d = QNodePrivate::get(m_root);
+    return d->m_scene ? d->m_scene->lookupNode(id) : nullptr;
+}
+
+QVector<QNode *> QAspectManager::lookupNodes(const QVector<QNodeId> &ids) const
+{
+    if (!m_root)
+        return {};
+
+    QNodePrivate *d = QNodePrivate::get(m_root);
+    return d->m_scene ? d->m_scene->lookupNodes(ids) : QVector<QNode *>{};
 }
 
 /*!
