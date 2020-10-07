bcad723d9c657ab16acd7ae0d31d3b2de512afd4
Mike Krus <mike.krus@kdab.com>
2020-10-07 10:20:57 +0100

    Use internal CMake API
    Task-number: QTBUG-86815
    Change-Id: I7a8755571c8977be1f16b2287e5a8055e9016ee5
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- tests/auto/core/common/common.cmake
+++ tests/auto/core/common/common.cmake
@@ -3,7 +3,7 @@ function(qt3d_setup_common_test target)
     target_sources(${target} PRIVATE ${commonDir}/qbackendnodetester.h ${commonDir}/qbackendnodetester.cpp)
     target_include_directories(${target} PRIVATE ${commonDir})
 
-#    qt_extend_target(${target} CONDITION QT_FEATURE_private_tests
+#    qt_internal_extend_target(${target} CONDITION QT_FEATURE_private_tests
 #        SOURCES
 #            ${commonDir}/qbackendnodetester.cpp ${commonDir}/qbackendnodetester.h
 #            ${commonDir}/testarbiter.h
