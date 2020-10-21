4ab96961366a8486deed675b27cfbe243ffa2363
Michael Brasser <mbrasser@ford.com>
2020-10-21 13:03:12 -0500

    Document how the font is used for extruded text
    Pick-to: 5.15
    Change-Id: Ib18adc0809948092beaad6511bfddc68f22cc459
    Reviewed-by: Paul Lemire <paul.lemire@kdab.com>

--- src/extras/3dtext/qextrudedtextgeometry.cpp
+++ src/extras/3dtext/qextrudedtextgeometry.cpp
@@ -224,6 +224,10 @@ void QExtrudedTextGeometryPrivate::init()
  * \qmlproperty QFont ExtrudedTextGeometry::font
  *
  * Holds the font of the text.
+ *
+ * The geometry is normalized by the font's pointSize, so a larger pointSize
+ * will result in smoother, rather than larger, text. pixelSize should not
+ * be used.
  */
 
 /*!
@@ -451,6 +455,10 @@ QString QExtrudedTextGeometry::text() const
  * \property QExtrudedTextGeometry::font
  *
  * Holds the font of the text.
+ *
+ * The geometry is normalized by the font's pointSize, so a larger pointSize
+ * will result in smoother, rather than larger, text. pixelSize should not
+ * be used.
  */
 QFont QExtrudedTextGeometry::font() const
 {
