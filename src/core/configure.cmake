858145c3063c4d85c5a81e72d8b5559af3c3b93c
Mike Krus <mike.krus@kdab.com>
2020-12-11 15:23:43 +0000

    Fix feature names in cmake
    Feature names were mistakenly changed to use underscores rather
    than dashes. This breaks builds of apps using qmake as it changes
    the feature detection symbols.
    
    Fixed by reverting to use dashes in the names. This is fine in cmake
    as dashes are converted to underscores when not quoted.
    
    Unfortunately, it changes the command line configuration arguments back
    to the dashed versions.
    
    [ChangeLog] Configuration time command line arguments have changed to
    use dashes rather than underscores
    
    Change-Id: If793c5424aa0459daf06dd075e2e40c33997ca1a
    Task-number: QTBUG-89386
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>
    (cherry picked from commit 86eb86514e6b895827e9b73a3bccc057081a470f)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/core/configure.cmake
+++ src/core/configure.cmake
@@ -16,39 +16,39 @@ set_property(CACHE INPUT_assimp PROPERTY STRINGS undefined qt system no)
 
 #### Features
 
-qt_feature("qt3d_render" PUBLIC
+qt_feature("qt3d-render" PUBLIC
     SECTION "Aspects"
     LABEL "Render aspect"
     PURPOSE "Use the 3D Render Aspect library"
 )
 
-qt_feature("qt3d_input" PUBLIC
+qt_feature("qt3d-input" PUBLIC
     SECTION "Aspects"
     LABEL "Input aspect"
     PURPOSE "Use the 3D Input Aspect library"
 )
 
-qt_feature("qt3d_logic" PUBLIC
+qt_feature("qt3d-logic" PUBLIC
     SECTION "Aspects"
     LABEL "Logic aspect"
     PURPOSE "Use the 3D Logic Aspect library"
 )
 
-qt_feature("qt3d_extras" PUBLIC
+qt_feature("qt3d-extras" PUBLIC
     SECTION "Aspects"
     LABEL "Extras aspect"
     PURPOSE "Use the 3D Extra library"
     CONDITION QT_FEATURE_qt3d_render AND QT_FEATURE_qt3d_input AND QT_FEATURE_qt3d_logic
 )
 
-qt_feature("qt3d_animation" PUBLIC
+qt_feature("qt3d-animation" PUBLIC
     SECTION "Aspects"
     LABEL "Animation aspect"
     PURPOSE "Use the 3D Animation Aspect library"
     CONDITION QT_FEATURE_qt3d_render
 )
 
-qt_feature("qt3d_simd_sse2" PRIVATE
+qt_feature("qt3d-simd-sse2" PRIVATE
     LABEL "Use SSE2 instructions"
     PURPOSE "Use SSE2 SIMD instructions to accelerate matrix operations"
     AUTODETECT QT_FEATURE_sse2
@@ -56,7 +56,7 @@ qt_feature("qt3d_simd_sse2" PRIVATE
     DISABLE INPUT_qt3d_simd STREQUAL 'no' OR ( TEST_architecture_arch STREQUAL i386 )
 )
 
-qt_feature("qt3d_simd_avx2" PRIVATE
+qt_feature("qt3d-simd-avx2" PRIVATE
     LABEL "Use AVX2 instructions"
     PURPOSE "Use AVX2 SIMD instructions to accelerate matrix operations"
     AUTODETECT TEST_arch_${TEST_architecture_arch}_subarch_avx2
@@ -65,11 +65,11 @@ qt_feature("qt3d_simd_avx2" PRIVATE
 )
 
 qt_configure_add_summary_section(NAME "Qt3D Core")
-qt_configure_add_summary_entry(ARGS "qt3d_simd_sse2")
-qt_configure_add_summary_entry(ARGS "qt3d_simd_avx2")
-qt_configure_add_summary_entry(ARGS "qt3d_animation")
-qt_configure_add_summary_entry(ARGS "qt3d_extras")
-qt_configure_add_summary_entry(ARGS "qt3d_input")
-qt_configure_add_summary_entry(ARGS "qt3d_logic")
-qt_configure_add_summary_entry(ARGS "qt3d_render")
+qt_configure_add_summary_entry(ARGS "qt3d-simd-sse2")
+qt_configure_add_summary_entry(ARGS "qt3d-simd-avx2")
+qt_configure_add_summary_entry(ARGS "qt3d-animation")
+qt_configure_add_summary_entry(ARGS "qt3d-extras")
+qt_configure_add_summary_entry(ARGS "qt3d-input")
+qt_configure_add_summary_entry(ARGS "qt3d-logic")
+qt_configure_add_summary_entry(ARGS "qt3d-render")
 qt_configure_end_summary_section() # end of "Qt3D" section
