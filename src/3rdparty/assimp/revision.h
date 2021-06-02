6f55fd683a0a9128408a5811fc010ff78d5830c4
Jani Heikkinen <jani.heikkinen@qt.io>
2021-06-02 14:13:27 +0300

    Update assimp submodule
    Change-Id: Ifee3593b8b55ce8ccbdd9cae57f290b1117303d6
    Reviewed-by: Jonas Karlsson <jonas.karlsson@qt.io>
    Reviewed-by: Mike Krus <mike.krus@kdab.com>

--- src/3rdparty/assimp/revision.h
+++ src/3rdparty/assimp/revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0xb3e1ee3c
+#define GitVersion 0x5a38cd0a
 #define GitBranch "HEAD"
 
 #define VER_MAJOR 5
@@ -16,7 +16,7 @@
 #if (GitVersion == 0)
 #define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD)
 #else
-#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit b3e1ee3c)"
+#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit 5a38cd0a)"
 #endif
 
 #ifdef  NDEBUG
