91974f11f12091d94d5c5f85a3368aa1844ee580
Iikka Eklund <iikka.eklund@qt.io>
2021-06-24 09:45:52 +0300

    Conan: Inherit recipe class from QtLeafModule for common functionality
    The recipe uses Conan's supported way to inherit from a given
    base class via 'python_requires_extend'.
    
    The qt-conan-common package implements a base class for Qt leaf
    module recipes. The build steps in leaf modules are mostly identical
    so it makes sense to put those in the base class.
    
    Dependencies are read by the base class from the 'dependencies.yaml'
    which is the same file the CI system uses.
    
    Pick-to: 6.2
    Task-number: QTBUG-94758
    Change-Id: I61baf3f96a027ee374af6b16ba370fb85a2e698c
    Reviewed-by: Jani Heikkinen <jani.heikkinen@qt.io>
    Reviewed-by: Toni Saario <toni.saario@qt.io>

--- conanfile.py
+++ conanfile.py
@@ -26,117 +26,55 @@
 ##
 #############################################################################
 
-from conans import ConanFile, tools, CMake
-import os
+from conans import ConanFile
+import re
 from pathlib import Path
+from typing import Dict, Any
+
+_qt3d_features = [
+    "qt3d-animation",
+    "qt3d-extras",
+    "qt3d-input",
+    "qt3d-logic",
+    "qt3d-opengl-renderer",
+    "qt3d-render",
+    "qt3d-rhi-renderer",
+    "qt3d-simd-avx2",
+    "qt3d-simd-sse2",
+]
 
 
-class QtConanError(Exception):
-    pass
+def _parse_qt_version_by_key(key: str) -> str:
+    with open(Path(__file__).parent.resolve() / ".cmake.conf") as f:
+        m = re.search(fr'{key} .*"(.*)"', f.read())
+    return m.group(1) if m else ""
+
+
+def _get_qt_minor_version() -> str:
+    return ".".join(_parse_qt_version_by_key("QT_REPO_MODULE_VERSION").split(".")[:2])
 
 
 class Qt3D(ConanFile):
     name = "qt3d"
-    version = "6.2.0"
-    license = "LGPL-3.0+, GPL-2.0+, Commercial Qt License Agreement"
+    license = "LGPL-3.0, GPL-2.0+, Commercial Qt License Agreement"
     author = "The Qt Company <https://www.qt.io/contact-us>"
-    url = "https://code.qt.io/cgit/qt/qt3d.git/"
-    description = "Qt 3D provides functionality for near-realtime simulation systems with support for 2D and 3D rendering in both Qt C++ and Qt Quick applications."
-    topics = ("qt", "qt6", "3d", "qtquick")
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False, "default"],
-               "qt6": "ANY"}  # this is needed to model unique package_id for the Add-on build per used Qt6 version
-    default_options = {"shared": "default",  # default: Use the value of the Qt build
-                       "qt6": None}
+    url = "https://code.qt.io/cgit/qt/qt3d.git"
+    description = (
+        "Qt 3D provides functionality for near-realtime simulation systems with support "
+        "for 2D and 3D rendering in both Qt C++ and Qt Quick applications."
+    )
+    topics = "qt", "qt6", "3d", "qtquick"
+    settings = "os", "compiler", "arch", "build_type"
+    # for referencing the version number and prerelease tag and dependencies info
+    exports = ".cmake.conf", "dependencies.yaml"
     exports_sources = "*", "!conan*.*"
-    # use commit ID as the RREV (recipe revision) if this is exported from .git repository
-    revision_mode = "scm" if Path(Path(__file__).parent.resolve() / ".git").exists() else "hash"
+    python_requires = f"qt-conan-common/{_get_qt_minor_version()}@qt/everywhere"
+    python_requires_extend = "qt-conan-common.QtLeafModule"
 
-    def source(self):
-        # sources are installed next to recipe, no need to clone etc. sources here
-        pass
+    def get_qt_leaf_module_options(self) -> Dict[str, Any]:
+        """Implements abstractmethod from qt-conan-common.QtLeafModule"""
+        return {item.replace("-", "_"): ["yes", "no", None] for item in _qt3d_features}
 
-    def _get_cmake_prefix_path(self):
-        # 'QTDIR' provided as env variable in profile file which is part of the Qt essential binary
-        # package(s). Installed under .conan/profiles
-        cmake_prefix_path = os.environ.get("QTDIR")
-        if not cmake_prefix_path:
-            raise QtConanError("'QTDIR' not defined! The 'QTDIR' needs to point to Qt installation directory.")
-        print(f"CMAKE_PREFIX_PATH for '{self.name}/{self.version}' build is: {cmake_prefix_path}")
-        return cmake_prefix_path
-
-    def _read_env(self, key):
-        value = os.environ.get(key)
-        if not value:
-            raise QtConanError(f"{self.settings.os} build specified but '{key}' was not defined?")
-        return value
-
-    def _get_qtcmake(self):
-        qt_install_path = self._get_cmake_prefix_path()
-        ext = ".bat" if tools.os_info.is_windows else ""
-        qtcmake = os.path.abspath(os.path.join(qt_install_path, "bin", "qt-cmake" + ext))
-        if not os.path.exists(qtcmake):
-            raise QtConanError(f"Unable to locate {qtcmake} from 'QTDIR': {qt_install_path}")
-        return qtcmake
-
-    def _get_cmake_tool(self):
-        cmake = CMake(self, cmake_program=self._get_qtcmake())
-        cmake.verbose = True
-
-        # Qt modules need to be 'installed'.
-        # We need to direct the 'make install' to some directory under Conan cache,
-        # place it under the current build directory which is also under the Conan cache.
-        # Note, the actual 'make install' is called in "package()".
-        install_dir = os.path.join(os.getcwd(), "_install_tmp")
-        cmake.definitions["CMAKE_INSTALL_PREFIX"] = install_dir
-
-        # Use the value of the Qt build
-        if self.options.shared.value == "default":
-            del cmake.definitions["BUILD_SHARED_LIBS"]
-
-        cmake_toolchain_file = os.environ.get("CMAKE_TOOLCHAIN_FILE")
-        if cmake_toolchain_file:
-            cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = cmake_toolchain_file
-
-        if self.settings.os == "Android":
-            cmake.definitions["ANDROID_SDK_ROOT"] = self._read_env('ANDROID_SDK_ROOT')
-            cmake.definitions["ANDROID_NDK_ROOT"] = self._read_env('ANDROID_NDK_ROOT')
-
-        if self.settings.os == "iOS":
-            # Instead of Conan's auto-added 'Darwin', explicitly pass 'iOS'.
-            cmake.definitions["CMAKE_SYSTEM_NAME"] = "iOS"
-
-            # Remove the explicit sysroot, let CMake detect the sysroots, to ensure
-            # that multi-arch builds work.
-            del cmake.definitions["CMAKE_OSX_SYSROOT"]
-
-            # Remove the conan provided architecture, instead rely on the architectures set
-            # by the Qt toolchain file, which with official Qt packages most likely means
-            # multi-arch iOS.
-            del cmake.definitions["CMAKE_OSX_ARCHITECTURES"]
-
-        for c in [ 'assimp', 'opengl_renderer', 'rhi_renderer']:
-            value = os.environ.get('qt3d_' + c)
-            if value:
-                cmake.definitions["QT_FEATURE_qt3d_" + c] = value
-
-        return cmake
-
-    def build(self):
-        cmake = self._get_cmake_tool()
-        self.run('%s "%s" %s' % (self._get_qtcmake(), self.source_folder, cmake.command_line))
-        self.run('cmake --build . %s' % cmake.build_config)
-        return
-
-    def package(self):
-        install_dir = os.path.join(os.getcwd(), "_install_tmp")  # see 'CMAKE_INSTALL_PREFIX' above
-        self.run('cmake --build . --target install')
-        self.copy("*", src=install_dir, dst=".")
-
-    def package_info(self):
-        self.cpp_info.libs = ["Qt63D"]  # used for the actual library filename, Ordered list with the library names
-
-    def deploy(self):
-        self.copy("*")  # copy from current package
-        self.copy_deps("*")  # copy from dependencies
-
+    def get_qt_leaf_module_default_options(self) -> Dict[str, Any]:
+        """Implements abstractmethod from qt-conan-common.QtLeafModule"""
+        return {item.replace("-", "_"): None for item in _qt3d_features}
