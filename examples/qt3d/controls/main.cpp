9f849d32179b53f93b89ccfc57742704afb8d54f
Mike Krus <mike.krus@kdab.com>
2023-02-22 14:39:44 +0000

    Merge both controls based examples
    Can toggle underlay mode from main.cpp.
    
    Change-Id: Id3ef156b0df9b30dd9d7d3620a2878e70434d69b
    Reviewed-by: Paul Lemire <paul.lemire@kdab.com>
    (cherry picked from commit 6c1155f6ac4e8baa554a55b915eca0add4030d07)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- examples/qt3d/controls/main.cpp
+++ examples/qt3d/controls/main.cpp
@@ -3,19 +3,29 @@
 
 #include <QGuiApplication>
 #include <QQuickView>
+#include <QQmlContext>
 #include <Qt3DRender/qt3drender-config.h>
 
 int main(int argc, char **argv)
 {
-    QGuiApplication app(argc, argv);
-#if !QT_CONFIG(qt3d_rhi_renderer)
-    qputenv("QSG_RHI_BACKEND", "opengl");
-#endif
+    const bool useUnderLayCompositingMode = false;
 
+    if (useUnderLayCompositingMode) {
+        // underLay compositing does not work with RHI renderer
+        qputenv("QT3D_RENDERER", "opengl");
+        qputenv("QSG_RHI_BACKEND", "opengl");
+    }
+
+    QSurfaceFormat format;
+    format.setSamples(4);
+    QSurfaceFormat::setDefaultFormat(format);
+
+    QGuiApplication app(argc, argv);
     QQuickView view;
 
     view.resize(520, 500);
     view.setResizeMode(QQuickView::SizeRootObjectToView);
+    view.rootContext()->setContextProperty("_useUnderLayCompositingMode", useUnderLayCompositingMode);
     view.setSource(QUrl("qrc:/main.qml"));
     view.show();
 
