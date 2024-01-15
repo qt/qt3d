d37732bceae240087deafb1c2d914ff21c846e6a
Mike Krus <mike.krus@kdab.com>
2024-01-15 11:19:15 +0000

    Address API review comments
    Added revision number to properties added in 6.7 and made sure all
    slots were tagged properly.
    
    Change-Id: I558824cb22fdf8d66c1f6859c5112ea912bdc21c
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 58c4843826f814556b80ce75cf36f5901e550ae6)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/extras/defaults/qorbitcameracontroller.h
+++ src/extras/defaults/qorbitcameracontroller.h
@@ -16,58 +16,41 @@ class Q_3DEXTRASSHARED_EXPORT QOrbitCameraController : public QAbstractCameraCon
 {
     Q_OBJECT
     Q_PROPERTY(float zoomInLimit READ zoomInLimit WRITE setZoomInLimit NOTIFY zoomInLimitChanged)
-    Q_PROPERTY(QVector3D upVector READ upVector WRITE setUpVector NOTIFY upVectorChanged)
-    Q_PROPERTY(bool inverseXTranslate READ inverseXTranslate WRITE setInverseXTranslate NOTIFY inverseXTranslateChanged)
-    Q_PROPERTY(bool inverseYTranslate READ inverseYTranslate WRITE setInverseYTranslate NOTIFY inverseYTranslateChanged)
-    Q_PROPERTY(bool inversePan READ inversePan WRITE setInversePan NOTIFY inversePanChanged)
-    Q_PROPERTY(bool inverseTilt READ inverseTilt WRITE setInverseTilt NOTIFY inverseTiltChanged)
-    Q_PROPERTY(bool zoomTranslateViewCenter READ zoomTranslateViewCenter WRITE setZoomTranslateViewCenter NOTIFY zoomTranslateViewCenterChanged)
+    Q_PROPERTY(QVector3D upVector READ upVector WRITE setUpVector NOTIFY upVectorChanged REVISION(6, 7))
+    Q_PROPERTY(bool inverseXTranslate READ inverseXTranslate WRITE setInverseXTranslate NOTIFY inverseXTranslateChanged REVISION(6, 7))
+    Q_PROPERTY(bool inverseYTranslate READ inverseYTranslate WRITE setInverseYTranslate NOTIFY inverseYTranslateChanged REVISION(6, 7))
+    Q_PROPERTY(bool inversePan READ inversePan WRITE setInversePan NOTIFY inversePanChanged REVISION(6, 7))
+    Q_PROPERTY(bool inverseTilt READ inverseTilt WRITE setInverseTilt NOTIFY inverseTiltChanged REVISION(6, 7))
+    Q_PROPERTY(bool zoomTranslateViewCenter READ zoomTranslateViewCenter WRITE setZoomTranslateViewCenter NOTIFY zoomTranslateViewCenterChanged REVISION(6, 7))
 
 public:
     explicit QOrbitCameraController(Qt3DCore::QNode *parent = nullptr);
     ~QOrbitCameraController();
 
     float zoomInLimit() const;
-
-    void setZoomInLimit(float zoomInLimit);
-
     QVector3D upVector() const;
-
-    void setUpVector(const QVector3D& upVector);
-
     bool inverseXTranslate() const;
-
-    void setInverseXTranslate(bool isInverse);
-
     bool inverseYTranslate() const;
-
-    void setInverseYTranslate(bool isInverse);
-
     bool inversePan() const;
-
-    void setInversePan(bool isInverse);
-
     bool inverseTilt() const;
-
-    void setInverseTilt(bool isInverse);
-
     bool zoomTranslateViewCenter() const;
 
+public Q_SLOTS:
+    void setZoomInLimit(float zoomInLimit);
+    void setUpVector(const QVector3D& upVector);
+    void setInverseXTranslate(bool isInverse);
+    void setInverseYTranslate(bool isInverse);
+    void setInversePan(bool isInverse);
+    void setInverseTilt(bool isInverse);
     void setZoomTranslateViewCenter(bool isTranslate);
 
 Q_SIGNALS:
     void zoomInLimitChanged();
-
     void upVectorChanged(const QVector3D &upVector);
-
     void inverseXTranslateChanged(bool isInverse);
-
     void inverseYTranslateChanged(bool isInverse);
-
     void inversePanChanged(bool isInverse);
-
     void inverseTiltChanged(bool isInverse);
-
     void zoomTranslateViewCenterChanged(bool isTranslate);
 
 protected:
@@ -85,3 +68,4 @@ private:
 QT_END_NAMESPACE
 
 #endif // QT3DEXTRAS_QORBITCAMERACONTROLLER_H
+
