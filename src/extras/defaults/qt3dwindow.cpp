fb1689daadfc6da428ebd86f911485d8c1dc4894
Lucie Gérard <lucie.gerard@qt.io>
2024-02-23 15:27:44 +0100

    Correct license for module and plugin files
    According to QUIP-18 [1], all module and
    plugin files should be
    LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
    or
    LicenseRef-Qt-Commercial OR GPL-3.0-only
    
    Also, LGPL and non LGPL should not be mixed in a given directory
    
    [1]: https://contribute.qt-project.org/quips/18
    
    Pick-to: 6.7
    Task-number: QTBUG-121787
    Change-Id: I0cf51fc0e516f200dfa6674c1168f16cf41ac149
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- src/extras/defaults/qt3dwindow.cpp
+++ src/extras/defaults/qt3dwindow.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2016 Klaralvdalens Datakonsult AB (KDAB).
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include "qt3dwindow.h"
 #include "qt3dwindow_p.h"
