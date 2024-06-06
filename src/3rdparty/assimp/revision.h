e2a98bd88bb5e08139ec62f1fe74f387de9f973b
Mike Krus <mike.krus@kdab.com>
2024-06-06 15:07:39 +0100

    Update Assimp to 5.4.1
    Task-number: QTBUG-126113
    Task-number: QTBUG-124891
    Change-Id: Ibcba94ea317681e7ea7834c956b29f301c0576cc
    Reviewed-by: Mike Krus <mike.krus@kdab.com>
    (cherry picked from commit 2e69ddd52ef74f1d84a2a93f07bf41bbf5c811f8)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/3rdparty/assimp/revision.h
+++ src/3rdparty/assimp/revision.h
@@ -1,12 +1,12 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0x8b9ed34e
+#define GitVersion 0x10df90ec
 #define GitBranch "HEAD"
 
 #define VER_MAJOR 5
 #define VER_MINOR 4
-#define VER_PATCH 0
+#define VER_PATCH 1
 #define VER_BUILD 0
 
 #define STR_HELP(x) #x
@@ -16,7 +16,7 @@
 #if (GitVersion == 0)
 #define VER_FILEVERSION_STR STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD)
 #else
-#define VER_FILEVERSION_STR STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit 5a38cd0a)"
+#define VER_FILEVERSION_STR STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit 10df90ec)"
 #endif
 
 #ifdef NDEBUG
