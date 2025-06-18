4aaabfe8c2ac0e7bc1c79a21e0b3dc05756cbeb8
Ulf Hermann <ulf.hermann@qt.io>
2025-06-18 11:44:51 +0200

    Simplify retrieval of ExecutionEngine from QQmlEngine
    getV4Engine() has been removed.
    
    Fixes: QTBUG-137866
    Change-Id: I882727cbe07a1355e1de9d5f51ebbcb39acb3d66
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

--- src/quick3d/quick3d/items/quick3dbuffer.cpp
+++ src/quick3d/quick3d/items/quick3dbuffer.cpp
@@ -93,7 +93,7 @@ void Quick3DBuffer::initEngines()
 {
     if (m_engine == nullptr) {
         m_engine = qmlEngine(parent());
-        m_v4engine = QQmlEnginePrivate::getV4Engine(m_engine);
+        m_v4engine = m_engine->handle();
     }
 }
 
