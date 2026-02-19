cafe5f8493f5859507e45a2a9069ce4740f9f9ab
Mike Krus <mike.krus@kdab.com>
2026-02-19 17:04:30 +0000

    Update Assimp
    [ChangeLog][Third-Party Code] Updated Assimp to 6.0.4
    
    Pick-to: 6.11.0 6.10 6.8 6.5
    Change-Id: I6068ff942beb6e486b884268c1c2d106502cc034
    Reviewed-by: Paul Lemire <paul.lemire@kdab.com>
    (cherry picked from commit f168f423109965e0b912eabf2ccaf59852d40130)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/3rdparty/assimp/revision.h
+++ src/3rdparty/assimp/revision.h
@@ -1,12 +1,12 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0xfb375dd8
+#define GitVersion 0x8ed088f2
 #define GitBranch "HEAD"
 
 #define VER_MAJOR 6
 #define VER_MINOR 0
-#define VER_PATCH 2
+#define VER_PATCH 4
 #define VER_BUILD 0
 
 #define STR_HELP(x) #x
@@ -16,7 +16,7 @@
 #if (GitVersion == 0)
 #define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD)
 #else
-#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit fb375dd8)"
+#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit 8ed088f2)"
 #endif
 
 #ifdef  NDEBUG
