9b348fa70cfcee0260fa5f1e141ee0dcf931e99d
Mike Krus <mike.krus@kdab.com>
2026-05-26 15:27:07 +0100

    Update assimp to 6.0.5
    [ChangeLog][Third-Party Code] Updated assimp to 6.0.5
    
    Pick-to: 6.11 6.10 6.9 6.8
    Bug-Report: QTBUG-146943
    Change-Id: Idd2a2ca29275d95ecc577e5c06d1217e9525d98f
    Reviewed-by: Paul Lemire <paul.lemire@kdab.com>

--- src/3rdparty/assimp/revision.h
+++ src/3rdparty/assimp/revision.h
@@ -1,12 +1,12 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0x8ed088f2
+#define GitVersion 0x0f600733
 #define GitBranch "HEAD"
 
 #define VER_MAJOR 6
 #define VER_MINOR 0
-#define VER_PATCH 4
+#define VER_PATCH 5
 #define VER_BUILD 0
 
 #define STR_HELP(x) #x
